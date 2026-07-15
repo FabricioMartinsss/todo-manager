db2b82943c86a81e9350c25d6336684e41621061
Fabrício M <fabmartins272@gmail.com>
2026-07-15 20:52:36 -0300

    refactor: remover armazenamento em memória

--- learning/todo-api/src/index.ts
+++ learning/todo-api/src/index.ts
@@ -7,28 +7,6 @@ app.use(express.json());  //middleware
 
 const port = 3412;
 
-type Tarefa = {
-    id: number;
-    titulo: string;
-    concluida: boolean;
-    dataCriacao: Date;
-}
-
-const tarefas: Tarefa[] = [
-    {
-        id: 1,
-        titulo: "Estudar Express",
-        concluida: false,
-        dataCriacao: new Date()
-    },
-    {
-        id: 2,
-        titulo: "Criar API TODO",
-        concluida: true,
-        dataCriacao: new Date()
-    }
-];
-
 function callback() {
     console.log("Servidor Iniciado");
 }
